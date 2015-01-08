f41f36b9a81dc09953733c2e27a8a8b0548ad120
bardo91 <19Bardo91@gmail.com>
2015-01-08 19:18:14 +0100

    Modified Divi's header.php to add two menus. One for navigation and the other for social networks. But has bad style.

--- wp-content/themes/Divi/header.php
+++ wp-content/themes/Divi/header.php
@@ -35,8 +35,14 @@
 
 	<?php wp_head(); ?>
 </head>
+
+<!-- Definition of header -->
+
+<!-- Create main container -->
 <body <?php body_class(); ?>>
 	<div id="page-container">
+	
+<!-- Load information to use in the header -->
 <?php
 	if ( is_page_template( 'page-template-blank.php' ) ) {
 		return;
@@ -60,126 +66,170 @@
 
 	$et_top_info_defined = $et_secondary_nav_items->top_info_defined;
 ?>
+<!-- -------------------------------------------------------------------------------------------------------------- -->
+<!-- Creation of headers -->
+<!-- -------------------------------------------------------------------------------------------------------------- -->
+<?php if ( $et_top_info_defined ) : ?>
+<!-- -------------------------------------------------------------------------------------------------------------- -->
+<!-- If there is info about top header, get that info and start it's construction -->
+<!-- -------------------------------------------------------------------------------------------------------------- -->
+	<div id="top-header" class="<?php echo esc_attr( $secondary_nav_class ); ?>">
+		<div class="container clearfix">
+<!-- -------------------------------------------------------------------------------------------------------------- -->
+<!-- If there is contact info... -->
+<!-- -------------------------------------------------------------------------------------------------------------- -->
+		<?php if ( $et_contact_info_defined ) : ?>
 
-	<?php if ( $et_top_info_defined ) : ?>
-		<div id="top-header" class="<?php echo esc_attr( $secondary_nav_class ); ?>">
-			<div class="container clearfix">
+			<div id="et-info">
+			<?php if ( '' !== ( $et_phone_number = et_get_option( 'phone_number' ) ) ) : ?>
+				<span id="et-info-phone"><?php echo esc_html( $et_phone_number ); ?></span>
+			<?php endif; ?>
 
-			<?php if ( $et_contact_info_defined ) : ?>
+			<?php if ( '' !== ( $et_email = et_get_option( 'header_email' ) ) ) : ?>
+				<span id="et-info-email"><?php echo esc_html( $et_email ); ?></span>
+			<?php endif; ?>
 
-				<div id="et-info">
-				<?php if ( '' !== ( $et_phone_number = et_get_option( 'phone_number' ) ) ) : ?>
-					<span id="et-info-phone"><?php echo esc_html( $et_phone_number ); ?></span>
-				<?php endif; ?>
-
-				<?php if ( '' !== ( $et_email = et_get_option( 'header_email' ) ) ) : ?>
-					<span id="et-info-email"><?php echo esc_html( $et_email ); ?></span>
-				<?php endif; ?>
-
-				<?php
-				if ( true === $show_header_social_icons ) {
-					get_template_part( 'includes/social_icons', 'header' );
-				} ?>
-				</div> <!-- #et-info -->
-
-			<?php endif; // true === $et_contact_info_defined ?>
-
-				<div id="et-secondary-menu">
-				<?php
-					if ( ! $et_contact_info_defined && true === $show_header_social_icons ) {
-						get_template_part( 'includes/social_icons', 'header' );
-					} else if ( $et_contact_info_defined && true === $show_header_social_icons ) {
-						ob_start();
-
-						get_template_part( 'includes/social_icons', 'header' );
-
-						$duplicate_social_icons = ob_get_contents();
-
-						ob_end_clean();
-
-						printf(
-							'<div class="et_duplicate_social_icons">
-								%1$s
-							</div>',
-							$duplicate_social_icons
-						);
-					}
-
-					if ( '' !== $et_secondary_nav ) {
-						echo $et_secondary_nav;
-					}
-
-					et_show_cart_total();
-				?>
-				</div> <!-- #et-secondary-menu -->
-
-			</div> <!-- .container -->
-		</div> <!-- #top-header -->
-	<?php endif; // true ==== $et_top_info_defined ?>
-
-		<header id="main-header" class="<?php echo esc_attr( $primary_nav_class ); ?>">
-			<div class="container clearfix">
 			<?php
-				$logo = ( $user_logo = et_get_option( 'divi_logo' ) ) && '' != $user_logo
-					? $user_logo
-					: $template_directory_uri . '/images/logo.png';
+			if ( true === $show_header_social_icons ) {
+				get_template_part( 'includes/social_icons', 'header' );
+			} ?>
+			</div> <!-- #et-info -->
+
+		<?php endif; // true === $et_contact_info_defined ?>
+<!-- -------------------------------------------------------------------------------------------------------------- -->
+<!-- Here is used the information about te secondary menu -->
+<!-- But (in Domocracy) we want to use this menu as part of the main header  so this fragment of code will be commented -->
+<!-- -------------------------------------------------------------------------------------------------------------- -->
+			<div id="et-secondary-menu">
+			<?php /**	---> Omitted
+				if ( ! $et_contact_info_defined && true === $show_header_social_icons ) {
+					get_template_part( 'includes/social_icons', 'header' );
+				} else if ( $et_contact_info_defined && true === $show_header_social_icons ) {
+					ob_start();
+
+					get_template_part( 'includes/social_icons', 'header' );
+
+					$duplicate_social_icons = ob_get_contents();
+
+					ob_end_clean();
+
+					printf(
+						'<div class="et_duplicate_social_icons">
+							%1$s
+						</div>',
+						$duplicate_social_icons
+					);
+				}
+
+				if ( '' !== $et_secondary_nav ) {
+					echo $et_secondary_nav;
+				}
+
+				et_show_cart_total();
+				<--- Omitted
+				*/
 			?>
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-					<img src="<?php echo esc_attr( $logo ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" id="logo" />
-				</a>
+			</div> <!-- #et-secondary-menu -->
 
-				<div id="et-top-navigation">
-					<nav id="top-menu-nav">
+		</div> <!-- .container -->
+	</div> <!-- #top-header -->
+<?php endif; // true ==== $et_top_info_defined ?>
+
+	<header id="main-header" class="<?php echo esc_attr( $primary_nav_class ); ?>">
+		<div class="container clearfix">
+		<?php
+			$logo = ( $user_logo = et_get_option( 'divi_logo' ) ) && '' != $user_logo
+				? $user_logo
+				: $template_directory_uri . '/images/logo.png';
+		?>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+				<img src="<?php echo esc_attr( $logo ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" id="logo" />
+			</a>
+<!-- -------------------------------------------------------------------------------------------------------------- -->
+<!-- Container with both primary and secondary menus -->
+<!-- -------------------------------------------------------------------------------------------------------------- -->
+			<div id="et-top-navigation">
+
+<!-- -------------------------------------------------------------------------------------------------------------- -->
+<!-- navigation for primary menu -->
+<!-- -------------------------------------------------------------------------------------------------------------- -->
+				<nav id="top-menu-nav">
+				<?php
+					$menuClass = 'nav';
+					if ( 'on' == et_get_option( 'divi_disable_toptier' ) ) $menuClass .= ' et_disable_top_tier';
+					$primaryNav = '';
+					
+					
+					$primaryNav = wp_nav_menu( array( 'theme_location' => 'primary-menu', 'container' => '', 'fallback_cb' => '', 'menu_class' => $menuClass, 'menu_id' => 'top-menu', 'echo' => false ) );
+					
+					if ( '' == $primaryNav ) :
+				?>
+					<ul id="top-menu" class="<?php echo esc_attr( $menuClass ); ?>">
+						<?php if ( 'on' == et_get_option( 'divi_home_link' ) ) { ?>
+							<li <?php if ( is_home() ) echo( 'class="current_page_item"' ); ?>><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php esc_html_e( 'Home', 'Divi' ); ?></a></li>
+						<?php }; ?>
+
+						<?php show_page_menu( $menuClass, false, false ); ?>
+						<?php show_categories_menu( $menuClass, false ); ?>
+					</ul>
+				<?php
+					else :
+						echo( $primaryNav );
+					endif;
+				?>
+				</nav>
+<!-- -------------------------------------------------------------------------------------------------------------- -->
+<!-- navigation for secondary menu -->
+<!-- -------------------------------------------------------------------------------------------------------------- -->
+				<nav id="top-menu-nav">
+				<?php
+					$menuClass = 'nav';
+					if ( 'on' == et_get_option( 'divi_disable_toptier' ) ) $menuClass .= ' et_disable_top_tier';
+					$secondaryNav = '';
+					$secondaryNav = wp_nav_menu( array( 'theme_location' => 'secondary-menu', 'container' => '', 'fallback_cb' => '', 'menu_class' => $menuClass, 'menu_id' => 'top-menu', 'echo' => false ) );
+					if ( '' == $secondaryNav ) :
+				?>
+					<ul id="top-menu" class="<?php echo esc_attr( $menuClass ); ?>">
+						<?php if ( 'on' == et_get_option( 'divi_home_link' ) ) { ?>
+							<li <?php if ( is_home() ) echo( 'class="current_page_item"' ); ?>><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php esc_html_e( 'Home', 'Divi' ); ?></a></li>
+						<?php }; ?>
+
+						<?php show_page_menu( $menuClass, false, false ); ?>
+						<?php show_categories_menu( $menuClass, false ); ?>
+					</ul>
+				<?php
+					else :
+						echo( $secondaryNav );
+					endif;
+				?>
+				
+				<!-- End -->
+				<?php
+				if ( ! $et_top_info_defined ) {
+					et_show_cart_total( array(
+						'no_text' => true,
+					) );
+				}
+				?>
+
+				<?php if ( false !== et_get_option( 'show_search_icon', true ) ) : ?>
+				<div id="et_top_search">
+					<span id="et_search_icon"></span>
+					<form role="search" method="get" class="et-search-form et-hidden" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 					<?php
-						$menuClass = 'nav';
-						if ( 'on' == et_get_option( 'divi_disable_toptier' ) ) $menuClass .= ' et_disable_top_tier';
-						$primaryNav = '';
-
-						$primaryNav = wp_nav_menu( array( 'theme_location' => 'primary-menu', 'container' => '', 'fallback_cb' => '', 'menu_class' => $menuClass, 'menu_id' => 'top-menu', 'echo' => false ) );
-
-						if ( '' == $primaryNav ) :
+						printf( '<input type="search" class="et-search-field" placeholder="%1$s" value="%2$s" name="s" title="%3$s" />',
+							esc_attr_x( 'Search &hellip;', 'placeholder', 'Divi' ),
+							get_search_query(),
+							esc_attr_x( 'Search for:', 'label', 'Divi' )
+						);
 					?>
-						<ul id="top-menu" class="<?php echo esc_attr( $menuClass ); ?>">
-							<?php if ( 'on' == et_get_option( 'divi_home_link' ) ) { ?>
-								<li <?php if ( is_home() ) echo( 'class="current_page_item"' ); ?>><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php esc_html_e( 'Home', 'Divi' ); ?></a></li>
-							<?php }; ?>
+					</form>
+				</div>
+				<?php endif; // true === et_get_option( 'show_search_icon', false ) ?>
 
-							<?php show_page_menu( $menuClass, false, false ); ?>
-							<?php show_categories_menu( $menuClass, false ); ?>
-						</ul>
-					<?php
-						else :
-							echo( $primaryNav );
-						endif;
-					?>
-					</nav>
+				<?php do_action( 'et_header_top' ); ?>
+			</div> <!-- #et-top-navigation -->
+		</div> <!-- .container -->
+	</header> <!-- #main-header -->
 
-					<?php
-					if ( ! $et_top_info_defined ) {
-						et_show_cart_total( array(
-							'no_text' => true,
-						) );
-					}
-					?>
-
-					<?php if ( false !== et_get_option( 'show_search_icon', true ) ) : ?>
-					<div id="et_top_search">
-						<span id="et_search_icon"></span>
-						<form role="search" method="get" class="et-search-form et-hidden" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-						<?php
-							printf( '<input type="search" class="et-search-field" placeholder="%1$s" value="%2$s" name="s" title="%3$s" />',
-								esc_attr_x( 'Search &hellip;', 'placeholder', 'Divi' ),
-								get_search_query(),
-								esc_attr_x( 'Search for:', 'label', 'Divi' )
-							);
-						?>
-						</form>
-					</div>
-					<?php endif; // true === et_get_option( 'show_search_icon', false ) ?>
-
-					<?php do_action( 'et_header_top' ); ?>
-				</div> <!-- #et-top-navigation -->
-			</div> <!-- .container -->
-		</header> <!-- #main-header -->
-
-		<div id="et-main-area">
+	<div id="et-main-area">
