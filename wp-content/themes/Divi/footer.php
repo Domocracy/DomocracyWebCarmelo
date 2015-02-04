b1ef1f3773ce2a759134b398fc2988855b840bcd
bardo91 <19Bardo91@gmail.com>
2015-02-04 18:21:48 +0100

    Fixed position of text and bg in section "This is domocracy". Erased unused footer.

--- wp-content/themes/Divi/footer.php
+++ wp-content/themes/Divi/footer.php
@@ -23,18 +23,6 @@ if ( ! is_page_template( 'page-template-blank.php' ) ) : ?>
 				</div> <!-- #et-footer-nav -->
 
 			<?php endif; ?>
-
-				<div id="footer-bottom">
-					<div class="container clearfix">
-				<?php
-					if ( false !== et_get_option( 'show_footer_social_icons', true ) ) {
-						get_template_part( 'includes/social_icons', 'footer' );
-					}
-				?>
-
-						<!-- <p id="footer-info"><?php printf( __( 'Designed by %1$s | Powered by %2$s', 'Divi' ), '<a href="http://www.elegantthemes.com" title="Premium WordPress Themes">Elegant Themes</a>', '<a href="http://www.wordpress.org">WordPress</a>' ); ?></p> -->
-					</div>	<!-- .container -->
-				</div>
 			</footer> <!-- #main-footer -->
 		</div> <!-- #et-main-area -->
 
